ea74eec11d0a1bbaa6f1b4591f198da06c1050d4
dd90001 <1292644504@qq.com>
2021-06-21 12:21:54 +0800

    second commit

--- src/utils/currencyId.ts
+++ src/utils/currencyId.ts
@@ -1,7 +1,7 @@
 import { Currency, ETHER, Token } from '@wanswap/sdk'
 
 export function currencyId(currency: Currency): string {
-  if (currency === ETHER) return 'WAN'
+  if (currency === ETHER) return 'VDS'
   if (currency instanceof Token) return currency.address
   throw new Error('invalid currency')
 }
